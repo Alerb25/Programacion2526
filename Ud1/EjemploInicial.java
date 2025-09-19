5ebeeb19b32d6d8583346f18ec734a7638303246
alejandra <aruizbermudez05@gmail.com>
2025-09-19 14:49:48 +0200

    modified:   Ud1/EjemploInicial.java

--- Ud1/EjemploInicial.java
+++ Ud1/EjemploInicial.java
@@ -1,5 +1,30 @@
 package Ud1;
 
 public class EjemploInicial {
-    
+  
+    public static void main(String args[]){
+        
+
+
+        /*para definir variables ponemos el tipo de dato que va a guardar 
+        y despues el nombre de la variable que lo definimos nostros */
+        int edad;
+        int edadInicial;
+        int edadPadres;
+
+        edad = 28;
+
+        //Variables de tipo decimal
+        float peso = 243.3f;
+        double pi = 3.149237856;
+
+        //cadenas de texto
+        String nombre = "Juan Pedro";
+
+        //codigo fuente
+        System.out.print("Bienvenido al curso 25-26 de programación!!");
+        
+
+    }
+
 }
