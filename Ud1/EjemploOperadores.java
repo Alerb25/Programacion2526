36c09757dd0ff15c136c79b815e26920cad0d6a4
alejandra <aruizbermudez05@gmail.com>
2025-09-22 13:46:16 +0200

    modified:   Ud1/EjemploOperadores.java

--- Ud1/EjemploOperadores.java
+++ Ud1/EjemploOperadores.java
@@ -1,37 +1,44 @@
 package Ud1;
 
 public class EjemploOperadores {
-    
+
     public static void main(String[] args) {
-        
+
         // Valores de ejemplo
         int edadHijo = 9;
-        int edadPadre = 37; 
+        int edadPadre = 37;
 
-        //operaciones básicas
+        // operaciones básicas
         int sumaEdades = 0;
         sumaEdades = edadHijo + edadPadre;
         int diferenciaEdades = edadPadre - edadHijo;
-        int media = sumaEdades /2;
-        int dobleHijo = edadHijo *2;
-        
-        /*El operador % es el resto de ka duvusuón entera,
-        en este caso 33/5 son y sobra 3, es decir, el resto son 3 */
-        int resto = 33%5;
-        
-        //Imprimiendo en la terminal
+        int media = sumaEdades / 2;
+        int dobleHijo = edadHijo * 2;
+
+        /*
+         * El operador % es el resto de ka duvusuón entera,
+         * en este caso 33/5 son y sobra 3, es decir, el resto son 3
+         */
+        int resto = 33 % 5;
+
+        // Imprimiendo en la terminal
         System.out.println("La suma de las edades es: " + sumaEdades);
         System.out.println("La diferencia de edad es: " + diferenciaEdades);
         System.out.println("La media de las edades es: " + media);
         System.out.println("El doble de la edad del Hijo es: " + dobleHijo);
 
-        /*La siguientes  operaciones son equivalentes:
-        hace que se le sume lo que haya en la parte derecha*/
+        /*
+         * La siguientes operaciones son equivalentes:
+         * hace que se le sume lo que haya en la parte derecha
+         */
 
-        edadHijo = edadHijo +10;
+        edadHijo = edadHijo + 10;
         edadHijo += 10;
-        System.out.println("Edad del Padre: " +edadPadre);
-        
+
+        // cuando los ++ se ponen a la izq pone la edad y demues le suma 1
+        System.out.println("Edad de Padre: " + edadPadre++);
+        // si se pone a la derecha se suma y depues se muestra por pantalla
+        System.out.println("Edad de Padre: " + ++edadPadre);
 
     }
 
