67a302de3e77e56e7a8412e8740357a9133303c6
alejandra <aruizbermudez05@gmail.com>
2025-09-23 12:17:04 +0200

    modified:   Ud1/EjemploOperadoresComparadores.java

--- Ud1/EjemploOperadoresComparadores.java
+++ Ud1/EjemploOperadoresComparadores.java
@@ -7,15 +7,24 @@ public class EjemploOperadoresComparadores {
     public static void main(String[] args) {
 
         boolean puedeIrDelante = false;
-        int altura = 0;
+        int altura = 0, edad = 0; // Tambien se puede definir asi
 
-        //leer por teclado si se utiliza scanner hay que cerrarlo 
+        // leer por teclado si se utiliza scanner hay que cerrarlo
         Scanner teclado = new Scanner(System.in);
 
+        // lee la altura
         System.out.println("Introduce la altura del niño:");
-        altura =teclado.nextInt();
+        altura = teclado.nextInt();
 
-        puedeIrDelante = altura >=  135;
+        // lee la edad
+        System.out.println("Introduce la edad del niño:");
+        edad = teclado.nextInt();
+
+        /*
+         * asignamos una condicion a la variable
+         * Si la condicion es cierta vale true y sino false
+         */
+        puedeIrDelante = altura >= 135;
 
         System.out.println("La condicion puede ir delante es " + puedeIrDelante);
 
@@ -24,4 +33,3 @@ public class EjemploOperadoresComparadores {
     }
 
 }
-
