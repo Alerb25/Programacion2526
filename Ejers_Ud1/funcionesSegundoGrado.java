fab5f19bfb02545c93a3b610647971ce2777cb8d
Ale <aruizbermudez05@gamil.com>
2025-09-25 17:54:35 +0200

    new file:   Ejers_Ud1/funcionesSegundoGrado.java

---
+++ Ejers_Ud1/funcionesSegundoGrado.java
@@ -0,0 +1,30 @@
+package Ejers_Ud1;
+
+import java.util.Scanner;
+
+public class funcionesSegundoGrado {
+    //Ejercicio 11
+    //Realiza un programa que resuelva una ecuación de segundo grado grado de tipo ax²+bx+c, recibiendo a,b y c y que devuelva el resultado
+    public static void main(String arg[]){
+        //Defino las variables y lo pongo para resgistre las respuestas por teclado
+        double a,b,c; 
+        double resultado; //float no es compatible con el resultado de Math()
+        Scanner teclado = new Scanner(System.in);
+
+        //Explico el programa y pido que el usuario introduzca los parametros
+        System.out.println("El programa calcula el resultado de la ecuación de segundo grado grado de tipo ax²+bx+c");
+        System.out.println("Por favor, introduzca el primer valor: ");
+        a = teclado.nextFloat();
+        System.out.println("Por favor, introduzca el segundo valor: ");
+        b = teclado.nextFloat();
+        System.out.println("Por favor, introduzca el tercer valor: ");
+        c = teclado.nextFloat();
+
+        //Para sacar el resultado usamos la formula y para las raices cuadrdadas utilizamos el .math()
+        resultado = (-b + Math.sqrt((b*b)-4*a*c))/(2*a);
+        System.out.println("El resultado es: " + resultado);
+
+
+        teclado.close();
+    }
+}
