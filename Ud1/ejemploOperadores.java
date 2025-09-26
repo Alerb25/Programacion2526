0f8a129df98cbb3f1607e864a1adb4877b49d2d2
alejandra <aruizbermudez05@gmail.com>
2025-09-26 12:54:59 +0200

    new file:   Ud1/ejemploOperadores.java

---
+++ Ud1/ejemploOperadores.java
@@ -0,0 +1,16 @@
+package Ud1;
+
+public class ejemploOperadores {
+    public static void main(String[] args) {
+        int numVidas = 5, numNiveles=19;
+        String nombreJuego = "Mario Brus";
+        char nivelDificultad = 'A';
+
+        //JOptionPane para que se lea un valor por teclado pero como si fuera una ventana emergente, siempre devuelve string
+        //para convertir int a String  se pone Integer.valueOf(lo que se quiera convertir)
+        
+        //Para convertir tipos en java lo más común es usar las funciones parse y valueOF 
+        //Para comparar String se usa nobreString.equals(frase a la que sea igual)
+    }   
+
+}
