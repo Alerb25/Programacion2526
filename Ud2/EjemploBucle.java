777a259b02be3dc6f8e1a2af2ec165e964010d9d
alejandra <aruizbermudez05@gmail.com>
2025-10-07 11:18:03 +0200

    modified:   Ud2/EjemploBucle.java

--- Ud2/EjemploBucle.java
+++ Ud2/EjemploBucle.java
@@ -2,11 +2,19 @@ package Ud2;
 
 public class EjemploBucle {
      public static void main(String[] args) {
+     /* 
         int dado=10;
     while (dado<25) {
         System.out.println("Una tirada pasable, has sacado: " + dado);
         dado++;
     }
-    
+    */
+
+    int num = (int) (Math.random()*10+1);
+    int bucle = 0;
+    while (bucle < 11 ){
+        System.out.println( num + " * " +bucle+ " = " + (num *bucle));
+        bucle++;
+    }
     }
 }
