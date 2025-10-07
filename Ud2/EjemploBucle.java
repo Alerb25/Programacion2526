c1ab2a6bdac3a9cc4577741932ec401a716ab937
alejandra <aruizbermudez05@gmail.com>
2025-10-07 10:50:48 +0200

    modified:   Ud2/EjemploBucle.java

---
+++ Ud2/EjemploBucle.java
@@ -0,0 +1,11 @@
+package Ud2;
+
+public class EjemploBucle {
+     public static void main(String[] args) {
+        int dado=20;
+    while (dado>10) {
+        System.out.println("Una tirada pasable");
+    }
+    
+    }
+}
