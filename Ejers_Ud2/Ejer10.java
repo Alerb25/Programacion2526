72ebd2bbb3450a8f7136bf50b17eed6f23a4a40d
alejandra <aruizbermudez05@gmail.com>
2025-10-21 12:03:03 +0200

    modified:   Ejers_Ud2/Ejer10.java

--- Ejers_Ud2/Ejer10.java
+++ Ejers_Ud2/Ejer10.java
@@ -1,5 +1,12 @@
 package Ejers_Ud2;
 
 public class Ejer10 {
-    
+    public static void main(String[] args) {
+        /*
+         * El programa lee 2 numeros y un resultado y muestra la operación utilizada
+         * pueden ser suma, resta, multiplicación, división, resto, raíz cuadrada, potencia de 2 o indefinida.
+         * En caso de que no sea ninguna de las anteriores deberá de mostrar un mensaje de que es una operación indefinida. 
+         * Se mostrará por pantalla el mensaje una sóla vez
+         */
+    }
 }
