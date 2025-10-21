eef87fc476ff7e8eeda4ba8a62b701082f976939
alejandra <aruizbermudez05@gmail.com>
2025-10-21 11:09:28 +0200

    modified:   Ejers_Ud2/precioFinal.java

--- Ejers_Ud2/precioFinal.java
+++ Ejers_Ud2/precioFinal.java
@@ -1,5 +1,7 @@
 package Ejers_Ud2;
 
+import java.util.Scanner;
+
 public class precioFinal {
     public static void main(String[] args) {
         /*
@@ -24,13 +26,93 @@ public class precioFinal {
         System.out.println("--------------------------------");
         System.out.println("");
         System.out.println("Usted está comprando una entrada de cine cuyo precio base es 12€, pero, ofrecemos descuentos si cumple con las condiciones requeridas.");
+        System.out.println("El dependiente es un poco despistado así que le hará muchas preguntas.");
         System.out.println("");
         System.out.println("--------------------------------");
 
         //Definimos variables
         int edad, numEntradas;
-        boolean esEstudiante, esFestivo, tieneDiscapacidad;
-        String diaSemana;
+        String esEstudiante,  tieneDiscapacidad;
+        String diaSemana, esFestivo;
+        float precio = 12;
+
+        //le hacemos todas las preguntas al usuario
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cuántas entradas quieres?");
+        numEntradas= sc.nextInt();
+        System.out.println("Por favor, introduce tu edad");
+        edad = sc.nextInt();
+        System.out.println("¿Hoy es festivo?");
+        esFestivo = sc.nextLine();
+        System.out.println("¿Es estudiante?");
+        esEstudiante = sc.nextLine();
+        System.out.println("¿Qué día de la semana es hoy?");
+        diaSemana = sc.nextLine();
+        System.out.println("¿Tiene discapacidad?");
+        tieneDiscapacidad = sc.nextLine();
+
+        //Empezamos con los casos utilizaré tanto el switch como el if para que sea más cómodo
+        //si es festivo no se aplica nada así que no se cambia el precio.
+        if (esFestivo != "Si"){
+            //si se compra mas de una entrada en finde se aplica 10% de descuento pero es acumulable
+            if (numEntradas > 1){
+                switch (diaSemana) {
+                    case "Sábado":
+                        //se aplica el 10% de descuento
+                        precio = 10.8f;
+                        break;
+                    case "Domingo":
+                        precio = 10.8f;
+                        break;
+                    default:
+                        //no se aplica descuento
+                        break;
+                }
+            }
+
+            //si es miercoles y eres menor de 25 se aplica el 25%
+            if (edad < 25 && diaSemana == "Miercoles" || edad < 25 && diaSemana == "Miércoles" ){
+                //Se le aplica 25% de descuento
+                precio = 9;
+                 switch (diaSemana) {
+                    case "Sábado":
+                        //se aplica el 10% de descuento
+                        precio = 8.1f;
+                        break;
+                    case "Domingo":
+                        precio = 8.1f;
+                        break;
+                    default:
+                        //no se aplica descuento
+                        break;
+                }
+                System.out.println("El precio es " + precio + " euros.");
+            }
+            //Si es mayor de 60 o estudiante tiene 15% descuento
+            if (edad > 60 || esEstudiante == "Si"){
+                precio = 10.20f;
+                switch (diaSemana) {
+                    case "Sábado":
+                        //se aplica el 10% de descuento
+                        precio = 9.18f;
+                        break;
+                    case "Domingo":
+                        precio = 9.18f;
+                        break;
+                    default:
+                        //no se aplica descuento
+                        break;
+                }
+                System.out.println("El precio es " + precio + " euros.");
+            }
+           
+            //si tiene discapacidad es gratis
+            if (tieneDiscapacidad == "Si"){
+                System.out.println("La entrada es gratis.");
+            }
+        } else {
+            System.out.println("El precio es " + precio + "euros");
+        }
 
     }
 }
