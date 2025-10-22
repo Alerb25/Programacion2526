59d0b889bb58689418d56af65aed8c2d8dcfe37f
alejandra <aruizbermudez05@gmail.com>
2025-10-22 10:33:44 +0200

    new file:   Ejers_Ud2/Ejer11.java

---
+++ Ejers_Ud2/Ejer11.java
@@ -0,0 +1,22 @@
+package Ejers_Ud2;
+
+
+
+
+
+public class Ejer11 {
+    public static void main(String[] args) {
+        /*
+         * Usar la fecha actual del sistema, para obtener el mes actual
+         * si el mes ingresado es futur de este año mostrar "Faltan X meses para mes"
+         * si ya ha pasado mostrar mes fue hace x meses
+         * si es este mes decir que estamos en este mes
+         * Usar switch para convertir el número a nombre de mes
+         * Despues sacar un mensaje diciendo cuantos dias tiene el mes
+         */
+
+        
+         
+        
+    }
+}
