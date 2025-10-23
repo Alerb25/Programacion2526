7fd87253b19c328af086a9f5000183b9f809143b
Ale <aruizbermudez05@gamil.com>
2025-10-23 23:30:46 +0200

    new file:   Ejers_Ud2/Ejer12.java

---
+++ Ejers_Ud2/Ejer12.java
@@ -0,0 +1,16 @@
+package Ejers_Ud2;
+
+public class Ejer12 {
+    public static void main(String[] args) {
+        /*
+         * Simular un juego donde 2 jugadores lanzan 3 dados al azar
+         * se muestra la tirada por pantalla  
+         * Reglas:
+         * 1. por trio gana el trio mas alto (Ej 666>555)
+         * 2. Si uno tiene trio y el otro no gana el del trio
+         * 3. Si uno tiene pareja y el otro no gana el que tenga pareja (sin trios)
+         * 4. Si no hay ni parejas ni trios gana el que tenga la suma mas alta
+         * 5. Si hay empate -> "Empate técnico"
+         */
+    }
+}
