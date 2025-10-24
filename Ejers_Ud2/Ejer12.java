fd9643f5134fb9bbf59f18173d2353947c139298
Alejandra Ruiz Bermúdez <149586680+Alerb25@users.noreply.github.com>
2025-10-24 12:39:48 +0200

    Correct pair detection and winner announcement logic
    Fix logic errors in pair detection and comparison.

--- Ejers_Ud2/Ejer12.java
+++ Ejers_Ud2/Ejer12.java
@@ -28,15 +28,76 @@ public class Ejer12 {
          //Detectamos los trios
          boolean threesm1 = (dice1 == dice2 && dice1 == dice3);
          boolean threesm2 = (dice4 == dice5 && dice4 == dice6);
+         
+         //Le damos valor para poder compararlos en caso en el que ambos jugadores saquen trio
          int valThrsm1= threesm1 ? dice1 : 0;
          int valThrsm2= threesm2 ? dice4 : 0;
 
          //Detectamos pareja 
+         //Nota: en el ejer no se habla nada si ambos jugadores tienen pares pero voy a aplicar la misma logica si sacaran 2 trios
          boolean pair1 = !threesm1 && (dice1 == dice2 || dice2 == dice3 || dice1 == dice3);
          boolean pair2 = !threesm2 && (dice4 == dice5 || dice5 == dice6 || dice4 == dice6);
          int valPair1 = 0;
-         int valPair2 = 0;
+        
+         //Identificamos cual es la pareja y le damos un valor. 
+         if (pair1 = true){
+            if (dice1 == dice2){
+                valPair1 = dice1;
+            } else if (dice2 == dice3){
+                valPair1 = dice2;
+            } else if (dice1 == dice3){
+                valPair1 = dice3;
+            }
+         }
 
+         int valPair2 = 0;
+          if (pair2 = true){
+            if (dice4 == dice5){
+                valPair1 = dice4;
+            } else if (dice5 == dice6){
+                valPair1 = dice5;
+            } else if (dice4 == dice6){
+                valPair1 = dice6;
+            }
+         }
+
+        //Empezamos con los casos para ganar 2 trio, 2 pareja y luego si no hay ninguna la suma de los dados
+
+        if ( threesm1 == true && threesm2 == true){
+            if (valThrsm1 > valThrsm2){
+                System.out.println( "El ganador es el Jugador 1 porque ha sacado un trío mayor.");
+            }else if (valThrsm1 < valThrsm2){
+                System.out.println( "El ganador es el Jugador 2 porque ha sacado un trío mayor.");
+            }
+        }
+
+        if (pair1 == true && pair2 == true){
+            if (valPair1 > valPair2){
+                System.out.println( "El ganador es el Jugador 1 porque ha sacado una pareja mayor.");
+            } else if (valPair1 < valPair2){
+                System.out.println( "El ganador es el Jugador 2 porque ha sacado una pareja mayor.");
+            }
+        }
+
+        if ( !threesm1 && !threesm2 && !pair1 && !pair2){
+            if (add1 >  add2){
+                System.out.println( "El ganador es el Jugador 1 porque ha sacado un total más alto.");
+            } else if (add1 < add2){
+                System.out.println( "El ganador es el Jugador 2 porque ha sacado un total más alto.");
+            }
+        }
+        
+        if ( threesm1 == true && !threesm2){
+            System.out.println( "El ganador es el Jugador 1 porque ha sacado un trío.");
+        }else if (threesm2 == true && !threesm1){
+            System.out.println( "El ganador es el Jugador 2 porque ha sacado un trío.");
+        }
+
+        if ( pair1 == true && !pair2){
+            System.out.println( "El ganador es el Jugador 1 porque ha sacado una pareja.");
+        }else if (pair2 == true && !pair1){
+            System.out.println( "El ganador es el Jugador 2 porque ha sacado una pareja.");
+        }
          
     }
 }
