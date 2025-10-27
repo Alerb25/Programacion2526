39c1a6597a94165d0408d32f20bf1f5fee314201
Ale <aruizbermudez05@gamil.com>
2025-10-27 10:03:26 +0100

    modified:   Ejers_Ud2/Ejer9.java

--- Ejers_Ud2/Ejer9.java
+++ Ejers_Ud2/Ejer9.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 
 public class Ejer9 {
     public static void main(String[] args) {
+        /*
+         * Calcular precio final de una entrada de cine
+         * base 12€
+         * Si es miércoles y el cliente tiene menos de 25 años, descuento del 20%.
+         * Si tiene más de 60 años o es estudiante con carnet, descuento del 15%.
+         * Si compra 2 o más entradas y es fin de semana, aplica 10% adicional sobre el total.
+         * Si es día festivo, no aplica ningún descuento.
+         * Solo se aplica el mayor descuento posible, excepto el del “fin de semana + múltiples entradas”, que se suma al principal si aplica.
+         * Si el cliente tiene discapacidad acreditada, la entrada es gratuita.
+         */
+
         System.out.println("Programa Precio Final.");
         System.out.println("--------------------------------");
         System.out.println("");
@@ -11,7 +22,7 @@ public class Ejer9 {
         System.out.println("");
         System.out.println("--------------------------------");
 
-        //Definimos las variables
+        //Definimos las variables (como lo dicen en el enunciado)
         int edad, numEntradas;
         String esEstudiante, tieneDiscapacidad;
         String diaSemana, esFestivo;
