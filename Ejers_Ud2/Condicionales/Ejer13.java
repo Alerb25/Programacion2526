ccb41448b03be419ded1ba55f75f41f6a1ea14c5
alejandra <aruizbermudez05@gmail.com>
2025-10-27 10:09:36 +0100

    modified:   Ejers_Ud2/Condicionales/Ejer13.java 	deleted:    Ejers_Ud2/Ejer13.java

--- Ejers_Ud2/Condicionales/Ejer13.java
+++ Ejers_Ud2/Condicionales/Ejer13.java
@@ -1,71 +1,41 @@
-package Condicionales;
 import java.util.Scanner;
 
 public class Ejer13 {
     public static void main(String[] args) {
         /*
-         * Crear un programa conversor de monedas
-         * se recibe la cantidad, la moneda origen y la moneda destino
+         * Crear un programa que actúe como conversor de monedas, recibiendo la cantidad, la moneda origen y la destino. 
+         * El alumno elegirá tres monedas cualesquiera y podrá convertir entre cualquiera de ellas (pantallazo funcionando)
          */
 
-         //Le explicamos al usuario el programa
-        System.out.println("Programa Conversor de Monedas."); 
+         System.out.println("Programa Conversor de Monedas.");
         System.out.println("--------------------------------");
         System.out.println("");
-        System.out.println("El programa es un convertidor de monedas, podrá escoger entre Euros, Libras y Dólares.");
-        System.out.println("Usted solo deberá introducir la cantidad, la moneda original y a la que quiere convertir.");
+        System.out.println("Este programa funciona de tal manera que el usuario introduce una cantidad, la moneda de origen y la moneda a la que se quiere convertir.");
         System.out.println("");
         System.out.println("--------------------------------");
 
-        //Ahora definimos las variables 
-        double  coinDest=0, value=0;
-        String nameOrigin, nameDest;
-        
+        //Definimos variables
+        int valOrigin=0, valDest=0;
+        String coinOrigin, coinDest;
+        //Definimos las conversiones dado a que solo son 3 monedas
+
+
+        //pedimos la cantidad, y las dos monedas
         Scanner sc = new Scanner(System.in);
-
-        //Pedimos las variables al usuario
         System.out.println("Introduzca la cantidad: ");
-        value = sc.nextInt();
+        valOrigin = sc.nextInt();
 
-        System.out.println("Introduzca la moneda origen: ");
-        nameOrigin = sc.nextLine();
+        System.out.println("Introduzca la moneda Origen (Puede ser Euros, Libras o Dólares): ");
+        coinOrigin = sc.nextLine();
+        
+        System.out.println("Introduzca la moneda a la que lo quiere convertir(Puede ser Euros, Libras o Dólares): ");
+        coinDest=sc.nextLine();
+
+        //Empezamos con los casos
+        
         
 
-        System.out.println("Introduzca la moneda a convertir: ");
-        nameDest= sc.nextLine();
-
-        System.out.println("Calculando ....");
-        //Empezamos con las conversiones  
-        if ( nameOrigin.equalsIgnoreCase("euros") ){
-            if (nameDest.equalsIgnoreCase("libras")){
-                coinDest = value * 1.14;
-
-            }else if (nameDest.equalsIgnoreCase("dolares")){
-                coinDest = value * 1.16;
-            }
-        } 
-
-        if ( nameOrigin.equalsIgnoreCase("libras") ){
-            if (nameDest.equalsIgnoreCase("euros")){
-                coinDest = value * 0.87;
-
-            }else if (nameDest.equalsIgnoreCase("dolares")){
-                coinDest = value * 1.33;
-            }
-        }
-        
-        if ( nameOrigin.equalsIgnoreCase("dolares") ){
-            if (nameDest.equalsIgnoreCase("libras")){
-                coinDest = value * 0.75;
-
-            }else if (nameDest.equalsIgnoreCase("euros")){
-                coinDest = value * 0.86;
-            }
-        } 
-        System.out.println("");
-        System.out.println("El valor de " + value +" en " +nameOrigin+ " pasandolos a "+ nameDest + " es de " + coinDest  );
 
         sc.close();
-
     }
 }
