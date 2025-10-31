83c9525582dacf0384fbcb2b7aec6b11d00cd913
alejandra <aruizbermudez05@gmail.com>
2025-10-31 13:21:31 +0100

    new file:   Ud2/EjemploTriangulo.java 	new file:   Ud2/EjemploTrianguloTwo.java

---
+++ Ud2/EjemploTriangulo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EjemploTrianguloTwo {
+    public static void main(String[] args) {
+        /*
+         * Leer la base del triángulo 
+         */
+        int base;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("La base del triangulo: ");
+        base =  sc.nextInt();
+
+        //Dibujamos el triangulo
+        for (int i = 0; i<base; i++){
+            for (int j = 0; j < i; j++){
+                System.out.println("*");
+            }
+            System.out.println("");
+        }
+
+        sc.close();
+    }
+}
