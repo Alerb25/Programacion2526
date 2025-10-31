769909fea15b67c8f96b401a90cc93db3472cdab
alejandra <aruizbermudez05@gmail.com>
2025-10-31 13:15:21 +0100

    new file:   Ejers_Ud2/Bucles/Ejer3.java

---
+++ Ejers_Ud2/Bucles/Ejer3.java
@@ -0,0 +1,38 @@
+package Bucles;
+
+import java.util.Scanner;
+
+public class Ejer3 {
+    /*
+     * Una pizzeria quiere repartir N pizzas entre sus empleados de forma justa
+     * Lee el numero de pizzas
+     * el numero de empleados
+     * Se da Cuantas pizzas reciben cada empleado y si sobran
+     */
+
+    public static void main(String[] args) {
+        //Le explicamos el programa al usuario
+     System.out.println("Programa Repartidor de Pizza.");
+     System.out.println("--------------------------------");
+     System.out.println("");
+     System.out.println("En esta pizzeria somos generosos y nos gusta compartir, queremos que todos nuestros empleados tengan pizza por partes iguales");
+     System.out.println("Este programa se va a dedicar a dividir las pizzas que hay entre los empleados");
+     System.out.println("");
+     System.out.println("--------------------------------"); 
+    
+     //Definimos las variables
+        Scanner sc = new Scanner(System.in);
+     int employee = 0, pizzas=0, div=0;
+
+     System.out.println("¿Cuantos empleados hay?");
+     employee = sc.nextInt();
+     System.out.println("¿Cuantas pizzas hay?");
+     pizzas = sc.nextInt();
+
+     if (pizzas > employee){
+        
+     }
+
+    }
+
+}
