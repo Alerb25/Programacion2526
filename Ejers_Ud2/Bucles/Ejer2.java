8eedbe9fc831401e6859f304953249d582e05fa9
Ale <aruizbermudez05@gamil.com>
2025-10-31 11:22:50 +0100

    modified:   Ejers_Ud2/Bucles/Ejer2.java

--- Ejers_Ud2/Bucles/Ejer2.java
+++ Ejers_Ud2/Bucles/Ejer2.java
@@ -26,7 +26,8 @@ public class Ejer2 {
     Scanner sc  = new Scanner(System.in);
     
     //Definimos las variables necesarias
-    int numDice=0, faces=0;
+    int numDice=0, faces=0, firstRoll=0, add=0;
+    boolean allmax = true, alleq = true;
     
     System.out.println("¿Cuántos dados?");
     numDice = sc.nextInt();
@@ -36,9 +37,42 @@ public class Ejer2 {
         System.out.println("De cuanto es el valor del dado " + (i+1));
         faces = sc.nextInt();
 
+        //Que se vea la tirada 
         int diceRoll = (int) (Math.random()*faces+1);
+        System.out.println("El dado " + (i+1) + ": " + diceRoll + " de " + faces);
+        add += diceRoll;
+
+        //Para saber si hay tiradas iguales me guardo la primera tirada y la comparo cn la siguiente
+        if (i == 0){
+            firstRoll = diceRoll;
+        } else {
+            // la comparacion porque si la siguiente no es igual que la primera no son todas las tiradas el mismo numero
+            if (firstRoll != diceRoll){
+                alleq = false;
+            }
+        }
+
+        //por si no hay criticos naturales
+        if (diceRoll != faces){
+            allmax = false;
+        }
     }
 
+    //ahora los resultados 
+    System.out.println("");
+    System.out.println("Los Resultados: ");
+    System.out.println("Suma total: " + add);
+    System.out.println("");
+
+    if (alleq){
+        System.out.println("Hay tirada épica!.");
+    }else {
+        System.out.println("No ha habido tirada épica");
+    }
+
+    if (allmax){
+        System.out.println("Ha habido Crítico Natural");
+    }
 
     }
 }
