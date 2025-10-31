9ad249b4a8e6e21e5e00065ddb27a053010e5bd1
alejandra <aruizbermudez05@gmail.com>
2025-10-31 14:00:35 +0100

    new file:   Ud2/EjemploOpcionCorrecta.java

---
+++ Ud2/EjemploOpcionCorrecta.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class EjemploOpcionCorrecta {
+    public static void main(String[] args) {
+        /*
+         * Se realizara un men para preguntar cual es el destino final de la raza humana
+         * tendra 4 opciones
+         * si el usuario elige la op correcta mostrara un mensaje tipo has salvado el mundo
+         * sino se pondra un mensaje tipo cataclismo zombie vuelve a elegir 
+         * pista usar switch, el usuario responde con una letra 
+         */
+
+         String op;
+         Scanner sc = new Scanner(System.in);
+         System.out.println("A. salvar el mundo");
+         System.out.println("B. No salvar el mundo");
+         System.out.println("C. Apocalipsis Zombie");
+         System.out.println("D. Muerte y destruccion");
+         System.out.println("Dame una respuesta: ");
+         op = sc.nextLine();
+         boolean correct = false;
+
+         while ( correct == false ){
+                   
+            switch (op) {
+                case "a":
+                case "A":
+                System.out.println("Bien! Has salvado el mundo");
+                correct = true;
+                break;
+                case "B":
+                case "b":
+                    System.out.println("No has salvado el mundo :c"); 
+                    break;
+                case "C":
+                case "c":
+                    System.out.println("Hay un apocalipsis Zombie :c");
+                break;
+
+                case "D":
+                case "d":
+                System.out.println("¿Porque eligirias eso? :/");
+                break;
+            
+                default:
+                    System.err.println("No deberias ver esto");
+                    break;
+                    
+            }
+            if (op == "b" || op == "B" || op == "c" || op == "C" || op == "d" || op == "D"){
+                System.out.println("");
+                System.out.println("-----------------------");
+                System.out.println("A. salvar el mundo");
+                System.out.println("B. No salvar el mundo");
+                System.out.println("C. Apocalipsis Zombie");
+                System.out.println("D. Muerte y destruccion");
+                System.out.println("Dame una respuesta: ");
+                op = sc.nextLine();
+            }
+         }
+        
+               
+         
+    }
+}
