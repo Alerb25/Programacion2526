89a5adaac66ebba5a39c3f2984b846f246698074
alejandra <aruizbermudez05@gmail.com>
2025-11-11 13:03:02 +0100

    new file:   Ejers_Ud2/Bucles/Ejer9.java

---
+++ Ejers_Ud2/Bucles/Ejer9.java
@@ -0,0 +1,12 @@
+package Bucles;
+
+public class Ejer9 {
+    public static void main(String[] args) {
+        /*
+         * Tenemos un array 8x8 que representa un tablero de hundir la flota
+         * debe comprobar si hay el numero de barcos correcto
+         * permite disparar y controlara si las cordenada estan entre 1 y 8
+         * Se respondera con Agua! o Tocado!
+         */
+    }
+}
