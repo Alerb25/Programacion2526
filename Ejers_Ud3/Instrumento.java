8023c4aefab9ab79d3e294ab82cbee9840f997f3
alejandra <aruizbermudez05@gmail.com>
2025-12-09 11:10:45 +0100

    modified:   Ejers_Ud3/Instrumento.java

--- Ejers_Ud3/Instrumento.java
+++ Ejers_Ud3/Instrumento.java
@@ -1,3 +1,14 @@
 public class Instrumento {
-    
+    //Atributos según el enunciado
+    private String nombre;
+    private int tipo;
+    private int volumen;
+    private double nivelDistorision;
+    private int rareza;
+    private boolean enUso;
+
+    //Atributos estáticos
+    public static final int GUITARRA_LASER = 1;
+
+
 }
