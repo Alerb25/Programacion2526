1cd9d64b7ba7287acec308081a50e0557853026b
Ale <aruizbermudez05@gamil.com>
2025-12-21 19:07:53 +0100

    modified:   Ejers_Ud3/Instrumento.java

--- Ejers_Ud3/Instrumento.java
+++ Ejers_Ud3/Instrumento.java
@@ -1,5 +1,5 @@
 public class Instrumento {
-    //Atributos según el enunciado
+    // Atributos según el enunciado
     private String nombre;
     private int tipo;
     private int volumen;
@@ -7,7 +7,7 @@ public class Instrumento {
     private int rareza;
     private boolean enUso;
 
-    //Atributos estáticos
+    // Atributos estáticos
     public static final int GUITARRA_LASER = 1;
     public static final int BATERIA_CUANTICA = 2;
     public static final int SINTETIZADOR_ESPACIAL = 3;
@@ -19,9 +19,8 @@ public class Instrumento {
     public static final int ROBADO = 3;
     public static final int ENCONTRADO_EN_MARTE = 4;
 
-
-    //Constructor de todos los parametros 
-    public Instrumento(String nombre, int tipo, int volumen, double nivelDistorision, int rareza, boolean enUso){
+    // Constructor de todos los parametros
+    public Instrumento(String nombre, int tipo, int volumen, double nivelDistorision, int rareza, boolean enUso) {
         this.nombre = nombre;
         this.tipo = tipo;
         this.volumen = volumen;
@@ -30,15 +29,65 @@ public class Instrumento {
         this.enUso = enUso;
 
     }
-    
-    //constructor con valores aleatorios (volumen entre 20-150, distorsion entre 0.0-10.0, uso = false)
-    public Instrumento(){
+
+    // constructor con valores aleatorios (volumen entre 20-150, distorsion entre
+    // 0.0-10.0, uso = false)
+    public Instrumento() {
         this.nombre = nombre;
         this.tipo = tipo;
-        this.volumen = (int) (Math.random() * (20-150+1))+20;
-        this.nivelDistorision = (int) (Math.random() * 10.0)+1.0;
+        this.volumen = (int) (Math.random() * (20 - 150 + 1)) + 20;
+        this.nivelDistorision = (int) (Math.random() * 10.0) + 1.0;
         this.rareza = rareza;
         this.enUso = false;
+    }
+
+    // getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public double getNivelDistorision() {
+        return nivelDistorision;
+    }
+
+    public int getRareza() {
+        return rareza;
+    }
+
+    public boolean getEnUso() {
+        return enUso;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setVolumen(int volumen) {
+        this.volumen = volumen;
+    }
+
+    public void setNivelDistorision(double nivelDistorision) {
+        this.nivelDistorision = nivelDistorision;
+    }
+
+    public void setRareza(int rareza) {
+        this.rareza = rareza;
+    }
+
+    public void setEnUso(boolean enUso) {
+        this.enUso = enUso;
     }
 
 }
