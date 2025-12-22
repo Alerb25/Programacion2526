7ea8d09f286104731c771a821199dd7cabff643f
Alejandra <aruizbermudez05@gmail.com>
2025-12-22 19:12:38 +0100

    modified:   Ejers_Ud3/Escenario.java

--- Ejers_Ud3/Escenario.java
+++ Ejers_Ud3/Escenario.java
@@ -3,4 +3,14 @@ public class Escenario {
     private String nombre;
     private int tipoEscenario;
     private int aforoMAx;
+    private boolean efectosEspeciales;
+
+    //Atributos estaticos
+    public static final int PRINCIPAL = 1, SECUNDARIO = 2, ACUSTICO = 3, UNDERGROUND = 4;
+
+    //Constructor vacio
+
+
+    //constructor con nombre tipo y aforo
+
 }
