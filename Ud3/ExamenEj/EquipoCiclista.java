af626ea6f1b8ceddecffa2e425fe7dc0b839808b
alejandra <aruizbermudez05@gmail.com>
2026-01-09 13:17:29 +0100

    modified:   Ud3/ExamenEj/EquipoCiclista.java 	modified:   Ud3/ExamenEj/PruebaCiclistas.java

--- Ud3/ExamenEj/EquipoCiclista.java
+++ Ud3/ExamenEj/EquipoCiclista.java
@@ -18,7 +18,9 @@ public class EquipoCiclista {
         this.id =  letraAleatoria() + String.valueOf((int) (Math.random()*100)+1);
         this.nombre = "";
         this.numCiclistas=(int) (Math.random()*90)+18;
-       for (int i = 0; i<10; i++){
+        this.listaCiclistas = new ArrayList<Ciclista>(); //imp definirlo primero para poder utilizarlo
+       //creammos 10 ciclistas aleatorios y los metemos en el equipo
+        for (int i = 0; i<10; i++){
             Ciclista ciclista = new Ciclista();
             listaCiclistas.add(ciclista);
        }
@@ -26,19 +28,20 @@ public class EquipoCiclista {
     }
 
     //constructor que recibe los parametros
-        public EquipoCiclista(String id, String nombre, int numCiclistas){
+        public EquipoCiclista(String id, String nombre, int numCiclistas, ArrayList<Ciclista> listaCiclistas){
     
         this.id = id;
         this.nombre = nombre;
         this.numCiclistas= numCiclistas;
+        this.listaCiclistas = listaCiclistas;
     
     }
 
     //get y set
-    public int getId() {
+    public String getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
     public String getNombre() {
@@ -47,6 +50,22 @@ public class EquipoCiclista {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public int getNumCiclistas() {
+        return numCiclistas;
+    }
+
+    public void setNumCiclistas(int numCiclistas) {
+        this.numCiclistas = numCiclistas;
+    }
+
+    public ArrayList<Ciclista> getListaCiclistas() {
+        return listaCiclistas;
+    }
+    public void setListaCiclistas(ArrayList<Ciclista> listaCiclistas) {
+        this.listaCiclistas = listaCiclistas;
+    }
+
   
 
         //metodo para obtener letras aleatorias
@@ -60,10 +79,10 @@ public class EquipoCiclista {
  
     @Override
     public String toString(){
-        return"ID: " + this.id +"\n"
+        return"Equipo Ciclista \n"+"ID: " + this.id +"\n"
         + "nombre: " + this.nombre +"\n"
         + "numero Ciclistas: " + this.numCiclistas +"\n"
-        +"\n";
+        + "lista de Ciclistas: " + this.listaCiclistas.toString() +"\n";
     }
 
 }
--- Ud3/ExamenEj/PruebaCiclistas.java
+++ Ud3/ExamenEj/PruebaCiclistas.java
@@ -1,7 +1,9 @@
 public class PruebaCiclistas {
     public static void main(String[] args) {
         
-       
+       EquipoCiclista equipo = new EquipoCiclista();
+
+       System.out.println(equipo);
 
 
     }
