ca7de1b48b6a58f7c5d704b176a35b0c9b1650db
alejandra <aruizbermudez05@gmail.com>
2026-01-12 14:40:13 +0100

    modified:   Ud4/peliculas/src/main/java/com/unidad4/EjemploConexiones.java 	modified:   Ud4/peliculas/target/classes/com/unidad4/EjemploConexiones.class

--- Ud4/peliculas/src/main/java/com/unidad4/EjemploConexiones.java
+++ Ud4/peliculas/src/main/java/com/unidad4/EjemploConexiones.java
@@ -1,12 +1,23 @@
 package com.unidad4;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class EjemploConexiones {
     public static void main(String[] args) {
+        Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (Exception e) {
+            connection = DriverManager.getConnection(
+                "jbdc:mysql://localhost:3306/mydb", "mydbuser", "mydbuser");
+        }catch(ClassNotFoundException ce){
+            System.out.println("Error bd");
+            ce.printStackTrace();
+        }
+         catch (Exception e) {
             // TODO: handle exception
             System.out.println("No tenemos el driver");
+            e.printStackTrace();
         }
     }
 }
