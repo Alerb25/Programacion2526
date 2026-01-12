8eaecd3b8644dddab9c1a8bd2f275fc987314848
Ale <aruizbermudez05@gamil.com>
2026-01-12 18:26:09 +0100

    computadora.java

--- Ud3/Examenes-ejemplo/Computadora/computadora.java
+++ Ud3/Examenes-ejemplo/Computadora/computadora.java
@@ -1,3 +1,34 @@
+import java.util.ArrayList;
+
 public class computadora {
-    
+    private String id;
+    public String nombre;
+    private int numComponentes;
+    private String tipo;
+    ArrayList<Componente>componentes; //se tiene que inicializar en le constructor
+
+    //Atributos estaticos
+    final static int TIPO_PORTATIL = 0;
+    final static int TIPO_SERVIDOR = 1;
+    final static int TIPO_GAMING = 2;
+
+    //constructor vacio
+
+
+    //constructor con parametros
+    public computadora(String id, String nombre, int numComponentes, String tipo, ArrayList<Componente> Componentes){
+        this.id = id;
+        this.nombre = nombre;
+        this.numComponentes = numComponentes;
+        this.tipo = tipo;
+        this.componentes = new ArrayList<Componente>(); 
+       //creammos 10 computadoras aleatorios y los metemos en el equipo
+        for (int i = 0; i<10; i++){
+            Computadora compu = new Computadora();
+            componentes.add(compu);
+       }
+    }
+
+     //metodos get y set
+
 }
