cfac971894c4e73cca8b0b7be5e32b9f8d880482
alejandra <aruizbermudez05@gmail.com>
2026-01-26 12:52:47 +0100

    modified:   Ejers_Ud4/bdd.sql

---
+++ Ejers_Ud4/bdd.sql
@@ -0,0 +1,25 @@
+-- Base de Datos
+create database proyecto; 
+create user proyecto identified by 'proyecto';
+grant select, insert, update, delete on proyecto.* to 'proyecto'@'localhost';
+
+use proyecto;
+
+
+
+
+-- Tablas
+--GnomoAnciano
+
+
+--TopoCombate
+
+--HadaMadrina
+
+--DuendeCombate
+
+--PicoPuntiagudo
+
+--VaritaMagica
+
+--CaracolGigante
