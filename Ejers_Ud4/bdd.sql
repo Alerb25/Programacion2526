a0450212d9ee8613db5fb0605f92ecaca4523645
alejandra <aruizbermudez05@gmail.com>
2026-01-26 13:04:10 +0100

    modified:   Ejers_Ud4/bdd.sql

--- Ejers_Ud4/bdd.sql
+++ Ejers_Ud4/bdd.sql
@@ -5,11 +5,18 @@ grant select, insert, update, delete on proyecto.* to 'proyecto'@'localhost';
 
 use proyecto;
 
-
-
-
 -- Tablas
 --GnomoAnciano
+CREATE TABLE gnomo (
+    id_Gnomo INT NOT NULL AUTO_INCREMENT, 
+    nombre VARCHAR(100),
+    apodoGuerra VARCHAR(100),
+    edad INT,
+    alturaBarba DECIMAL(5,2),
+    nivelCascarrabias INT,
+    energiaRefunfunno INT,
+    PRIMARY KEY (id_Gnomo)
+);
 
 
 --TopoCombate
