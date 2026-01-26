1ef150256b5744f2f2c39d96116d1dde4e741e95
Ale <aruizbermudez05@gamil.com>
2026-01-26 19:32:54 +0100

    modified:   Ejers_Ud4/bdd.sql

--- Ejers_Ud4/bdd.sql
+++ Ejers_Ud4/bdd.sql
@@ -24,17 +24,39 @@ CREATE TABLE topoCombate (
     id_Topo INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     apodoGuerra VARCHAR(100) NOT NULL,
+    edad INT NOT NULL,
     modelo VARCHAR(100)  NOT NULL,
     fuerzaExcavacion INT NOT NULL,
     agudezaOlfativa INT NOT NULL,
-    horasSuenno INT NOT NULL,
+    horasSuenno TIME NOT NULL,
     tiempoTierraEnOjos INT NOT NULL,
     PRIMARY KEY (id_Topo)
-)
+);
 
 --HadaMadrina
-
+CREATE TABLE hadaMadrina(
+    id_Hada INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    apodoGuerra VARCHAR(100) NOT NULL,
+    edad INT NOT NULL,
+    envergaduraAlas INT NOT NULL,
+    nivelPasivoAgresivo INT NOT NULL,
+    polvoPurpurina BOOLEAN NOT NULL,
+    PRIMARY KEY (id_Hada)
+);
 --DuendeCombate
+CREATE TABLE DuendeCombate (
+    id_Duende INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL, 
+    apodoGuerra VARCHAR(100) NOT NULL, 
+    modelo VARCHAR(500) NOT NULL, 
+    agilidad INT, 
+    nivelSarcasmo INT NOT NULL, 
+    horasSombra TIME NOT NULL, 
+    tiempoBrilloCegador TIME,
+    PRIMARY KEY ()
+    )
+
 
 --PicoPuntiagudo
 
