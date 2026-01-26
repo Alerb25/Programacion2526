35c5d4f041afc27dc8ac33294196db3678ac2f7a
alejandra <aruizbermudez05@gmail.com>
2026-01-26 13:59:09 +0100

    modified:   Ejers_Ud4/bdd.sql

--- Ejers_Ud4/bdd.sql
+++ Ejers_Ud4/bdd.sql
@@ -9,17 +9,28 @@ use proyecto;
 --GnomoAnciano
 CREATE TABLE gnomo (
     id_Gnomo INT NOT NULL AUTO_INCREMENT, 
-    nombre VARCHAR(100),
-    apodoGuerra VARCHAR(100),
-    edad INT,
-    alturaBarba DECIMAL(5,2),
-    nivelCascarrabias INT,
-    energiaRefunfunno INT,
+    nombre VARCHAR(100) NOT NULL,
+    apodoGuerra VARCHAR(100) NOT NULL,
+    edad INT NOT NULL,
+    alturaBarba DECIMAL(5,2) NOT NULL,
+    nivelCascarrabias INT NOT NULL,
+    energiaRefunfunno INT NOT NULL,
     PRIMARY KEY (id_Gnomo)
 );
 
 
 --TopoCombate
+CREATE TABLE topoCombate (
+    id_Topo INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    apodoGuerra VARCHAR(100) NOT NULL,
+    modelo VARCHAR(100)  NOT NULL,
+    fuerzaExcavacion INT NOT NULL,
+    agudezaOlfativa INT NOT NULL,
+    horasSuenno INT NOT NULL,
+    tiempoTierraEnOjos INT NOT NULL,
+    PRIMARY KEY (id_Topo)
+)
 
 --HadaMadrina
 
