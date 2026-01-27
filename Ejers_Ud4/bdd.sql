8797568eb0186d0822de72ee9bbb9aa6addedef3
alejandra <aruizbermudez05@gmail.com>
2026-01-27 11:04:22 +0100

    modified:   Ejers_Ud4/bdd.sql

--- Ejers_Ud4/bdd.sql
+++ Ejers_Ud4/bdd.sql
@@ -1,13 +1,13 @@
--- Base de Datos
+--  Base de Datos
 create database proyecto; 
 create user proyecto identified by 'proyecto';
 grant select, insert, update, delete on proyecto.* to 'proyecto'@'localhost';
 
 use proyecto;
 
--- Tablas
---ENTIDAES PRINCIPALES
---GnomoAnciano
+--  Tablas
+-- ENTIDAES PRINCIPALES
+-- GnomoAnciano
 CREATE TABLE gnomo (
     id_Gnomo INT NOT NULL AUTO_INCREMENT, 
     nombre VARCHAR(100) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE gnomo (
 );
 
 
---TopoCombate
+-- TopoCombate
 CREATE TABLE topoCombate (
     id_Topo INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE topoCombate (
     PRIMARY KEY (id_Topo)
 );
 
---HadaMadrina
+-- HadaMadrina
 CREATE TABLE hadaMadrina(
     id_Hada INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE hadaMadrina(
     polvoPurpurina BOOLEAN NOT NULL,
     PRIMARY KEY (id_Hada)
 );
---DuendeCombate
+-- DuendeCombate
 CREATE TABLE duendeCombate (
     id_Duende INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL, 
@@ -58,8 +58,8 @@ CREATE TABLE duendeCombate (
     PRIMARY KEY (idea_Duende)
     );
 
---ENTIDADES SECUNDARIAS
---PicoPuntiagudo
+-- ENTIDADES SECUNDARIAS
+-- PicoPuntiagudo
 CREATE TABLE picoPuntiagudo(
     id_Pico INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL, 
@@ -69,7 +69,7 @@ CREATE TABLE picoPuntiagudo(
     PRIMARY KEY (id_Pico)
 );
 
---VaritaMagica
+-- VaritaMagica
 CREATE TABLE varitaMagica(
     id_Varita INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL, 
@@ -79,7 +79,7 @@ CREATE TABLE varitaMagica(
     PRIMARY KEY (id_Pico)
 );
 
---CaracolGigante
+-- CaracolGigante
 CREATE TABLE caracolGigante(
     id_Caracol INT NOT NULL AUTO_INCREMENT,
     nunSerie INT NOT NULL, 
@@ -91,16 +91,16 @@ CREATE TABLE caracolGigante(
     PRIMARY KEY (id_Caracol)
 );
 
---RELACIONES
--- 1:n topo y gnomo
+-- RELACIONES
+--  1:n topo y gnomo
 alter table topoCombate add column fk_gnomo int;
 alter table topoCombate add constraint fk_topognomo foreign key(fk_gnomo) references gnomo(id_Gnomo);
 
--- 1:n hada y duende
+--  1:n hada y duende
 alter table duendeCombate add column fk_Hada int;
 alter table duendeCombate add constraint fk_DuendeHada foreign key(fk_Hada) references hadaMadrina(id_Hada);
 
--- n:m topo y picos
+--  n:m topo y picos
 create table toposPicos (
     id_ToposPicos INT NOT NULL AUTO_INCREMENT,
     fk_Topos INT,
@@ -110,7 +110,7 @@ create table toposPicos (
 alter table toposPicos add constraint fk_ToposPicos1 foreign key(fk_Topos) references topoCombate(id_Topo);
 alter table toposPicos add constraint fk_ToposPicos2 foreign key(fk_Picos) references picoPuntiagudo(id_Pico);
 
--- n:m duende y varita
+--  n:m duende y varita
 create table duendeVaritas (
     id_DuendeVaritas INT NOT NULL AUTO_INCREMENT,
     fk_Duende INT,
@@ -120,13 +120,13 @@ create table duendeVaritas (
 alter table duendeVaritas add constraint fk_DuendeVaritas1 foreign key(fk_Duende) references duendeCombate(id_Duende);
 alter table duendeVaritas add constraint fk_DuendeVaritas2 foreign key(fk_Varita) references varitaMagica(id_Varita);
 
--- 1:n topo y caracoles
+--  1:n topo y caracoles
 alter table caracolGigante add column fk_topo int;
 alter table caracolGigante add constraint fk_topoCaracol foreign key(fk_topo) references topoCombate(id_Topo);
 
--- 1:n duende y caracoles
+--  1:n duende y caracoles
 alter table caracolGigante add column fk_Duende int;
 alter table caracolGigante add constraint fk_duendeCaracol foreign key(fk_Duende) references DuendeCombate(id_Duende);
 
 
---INSERT
+-- INSERT
