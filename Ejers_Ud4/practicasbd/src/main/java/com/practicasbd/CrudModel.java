2ebf578f9db947102e4bd8645d952e19a22d5014
alejandra <aruizbermudez05@gmail.com>
2026-02-16 13:32:48 +0100

    con

--- Ejers_Ud4/practicasbd/src/main/java/com/practicasbd/CrudModel.java
+++ Ejers_Ud4/practicasbd/src/main/java/com/practicasbd/CrudModel.java
@@ -1,6 +1,10 @@
 package com.practicasbd;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
@@ -19,16 +23,30 @@ public class CrudModel {
 
         // Creamos la url de conexion con el formato para mysql
         String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
-
+        
         try {
             Connection con = null;
             Class.forName("com.mysql.cj.jdbc.Driver");
-            
+            con = DriverManager.getConnection(url, user, password);
+
+            // Creamos la consulta sql
+            //REVISAR COMO PROTEGER LOS DATOS 
+            String query = "select * from proyecto";
+
+            // Creamos la sentencia
+            Statement stmt = con.createStatement();
+
+            // Ejecutamos y guardamos los datos en un resultset
+            ResultSet rs = stmt.executeQuery(query);
+
             con.close();
         } catch (ClassNotFoundException ce) {
-            System.out.println("Error bd");
+            System.out.println("No tenemos el driver de conexion a bd instalado");
             ce.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Hubo un problema con la BD");
+            e.printStackTrace();
         }
-
     }
+
 }
