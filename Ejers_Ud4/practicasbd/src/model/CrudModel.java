8fdc094986c7a56384f41c800e0bc0bbeeae1c72
alejandra <aruizbermudez05@gmail.com>
2026-02-16 13:44:28 +0100

    cambios

--- Ejers_Ud4/practicasbd/src/model/CrudModel.java
+++ Ejers_Ud4/practicasbd/src/model/CrudModel.java
@@ -11,19 +11,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class CrudModel {
     public static void main(String[] args) throws SQLException {
 
-        // Al crear el dotenv cargamos todas las variables
-        Dotenv dotenv = Dotenv.load();
-
-        // Asigno las variables desde el fichero .env
-        String host = dotenv.get("DB_HOST");
-        String port = dotenv.get("DB_PORT");
-        String dbName = dotenv.get("DB_NAME");
-        String user = dotenv.get("DB_USER");
-        String password = dotenv.get("DB_PASSWORD");
-
-        // Creamos la url de conexion con el formato para mysql
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
-        
         try {
             Connection con = null;
             Class.forName("com.mysql.cj.jdbc.Driver");
