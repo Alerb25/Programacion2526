81548e26273fb20f4da5f35e700343871bf73cfa
alejandra <aruizbermudez05@gmail.com>
2026-04-21 11:23:19 +0200

    los cambios del pablo

--- Ud5/javafx-app/src/main/java/com/daw/app/App.java
+++ Ud5/javafx-app/src/main/java/com/daw/app/App.java
@@ -2,8 +2,11 @@ package com.daw.app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -14,11 +17,22 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        
+        //hemos creado una etuiqueta para el nombre
+        Label lblNombre = new Label("Nombre");
+        //input de texto pal usuario
+        TextField txtNombre = new TextField();
+        //boton cualquiera
+        Button btnAccion = new Button("Accion");
+        //panel para añadir los elementos
+        VBox vPanel = new VBox();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        //añadir los elementos a la escena
+        var scene = new Scene(vPanel);
+        
+        stage.setTitle("aplicación mutante");
+        stage.setHeight(500);
+        stage.setWidth(900);
         stage.setScene(scene);
         stage.show();
     }
