5654f45b0445f310b6cd4aa3246ffe3e4da557e6
alejandra <aruizbermudez05@gmail.com>
2026-04-21 12:57:05 +0200

    changes

--- Ud5/javafx-app/src/main/java/com/daw/app/App.java
+++ Ud5/javafx-app/src/main/java/com/daw/app/App.java
@@ -1,10 +1,15 @@
 package com.daw.app;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -21,24 +26,47 @@ public class App extends Application {
         //hemos creado una etuiqueta para el nombre
         Label lblNombre = new Label("Nombre");
         Label lblResultado = new Label();
+        Label lblBanner = new Label("App Mutante");
+        Label lblSlider = new Label();
+        lblBanner.setScaleX(4);
+        lblBanner.setScaleY(4);
+
         //input de texto pal usuario
         TextField txtNombre = new TextField();
         //boton cualquiera
         Button btnAccion = new Button("Accion");
+        Slider sldPuntuacion = new Slider(0, 100, 50);
           //panel para añadir los elementos
         VBox vPanel = new VBox();
-
-        btnAccion.setOnAction( e ->{
-            lblResultado.setText("Bienvenido a tu primera app " + txtNombre.getText());
-            vPanel.getChildren().add(lblResultado);
-        });
-      
+        BorderPane panelPrincipal = new BorderPane();
 
         //para que aparezcán las cosas  en el panel  
         vPanel.getChildren().addAll(lblNombre, txtNombre, btnAccion);
 
+       
+        btnAccion.setOnAction( e ->{
+            lblResultado.setText("Bienvenido a tu primera app " + txtNombre.getText());
+            vPanel.getChildren().add(lblResultado);
+        });
+        
+        sldPuntuacion.setOnMouseDragged(e ->{
+           lblSlider.setText("Valor:" + (int)sldPuntuacion.getValue());
+        });
+
+        txtNombre.setMaxWidth(200);
+        vPanel.setPadding(new Insets(20));
+        vPanel.setSpacing(10);
+        
+
+
+        panelPrincipal.setCenter(vPanel);
+        panelPrincipal.setPadding(new Insets(20));
+        panelPrincipal.setAlignment(lblBanner, Pos.CENTER);
+        panelPrincipal.setTop(lblBanner);
+        panelPrincipal.setBottom(sldPuntuacion);
+        panelPrincipal.setRight(lblSlider);
         //añadir los elementos a la escena
-        var scene = new Scene(vPanel);
+        var scene = new Scene(panelPrincipal);
         
         stage.setTitle("aplicación mutante");
         stage.setHeight(500);
